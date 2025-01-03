6c9b09a44c55e1b685edafda910a31c034740126
TDuy001 <nguyentienduy.tn2003gmail.com>
2025-01-03 20:30:46 +0700

    lưu thay đổi

--- obj/Debug/net9.0/Demo3.AssemblyInfo.cs
+++ obj/Debug/net9.0/Demo3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+521f14cce63127b79b8f068e5c357788de406fb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe3a9ecbef1592e52caf9d5f0112d17f868d7910")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
